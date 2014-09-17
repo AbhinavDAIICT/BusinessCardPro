7ac1a9b39842471f6c3b2dce2c6293384aa8575a
AbhinavDAIICT <abhinavtripathi01@hotmail.com>
2014-09-17 15:55:51 +0530

    First Commit
    Visual Studio default files for 'Windows Silverlight Apps'

---
+++ Business Card/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using Business_Card.Resources;
+
+namespace Business_Card
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
